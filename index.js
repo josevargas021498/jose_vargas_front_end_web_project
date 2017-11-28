4e1dcfd5f515a0d221b1f227fac0fa8d23e7fb3f
Jose Vargas <jvargas@basecampcodingacademy.org>
2017-11-28 11:05:30 -0600

    Function to take away from stock does not work, i dont know why. no good point.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var PAGE_DATA = {
 
         {
             imgUrl:
-                'https://pictures.dealer.com/l/liftedtrucks/0657/0694931c69e8d6493c0db071e09ab385x.jpg?impolicy=resize&w=320',
+                'http://pictures.dealer.com/b/brooksautocenter/1385/de5d68a490b9c6074a32b82bd9923ef2x.jpg?impolicy=resize&w=650',
             year: '2017',
             make: 'Chevrolet',
             model: 'Silverado 1500 z71',
@@ -30,7 +30,7 @@ var PAGE_DATA = {
         },
         {
             imgUrl:
-                'https://pictures.dealer.com/l/liftedtrucks/0657/0694931c69e8d6493c0db071e09ab385x.jpg?impolicy=resize&w=320',
+                'https://www.bulletproofsuspension.net/wp-content/uploads/2017/01/IMG_4204.jpg',
             year: '2017',
             make: 'Chevrolet',
             model: 'Silverado 1500 z71',
@@ -43,7 +43,7 @@ var PAGE_DATA = {
         },
         {
             imgUrl:
-                'https://pictures.dealer.com/l/liftedtrucks/0657/0694931c69e8d6493c0db071e09ab385x.jpg?impolicy=resize&w=320',
+                'https://i.pinimg.com/736x/3b/ca/89/3bca89f766c9e03bd90502619130f61d--chevy--lift.jpg',
             year: '2017',
             make: 'Chevrolet',
             model: 'Silverado 1500 z71',
@@ -55,7 +55,25 @@ var PAGE_DATA = {
             stock: 3
         },
         {
-            imgUrl: '',
+            imgUrl:
+                'http://www.car-revs-daily.com/wp-content/uploads/2015/01/Houston-Auto-Show-Customs-Top-10-LIFTED-TRUCKS-20.jpg',
+            year: '2017',
+            make: 'Chevrolet',
+            model: 'Tahoe 1500 z71',
+            engine: '6.0 Vortec Max',
+            mileage: '5,492',
+            description:
+                "Looking for a comfortable ride while traveling in the North Carolina trails? Look no further, our New Chevrolet Tohoe \
+            here will do the job!! 7 inch navigator radio and all around camera system monitor , leather seats, fully loaded and more!! 7.5 inch \
+            Rough Country suspension lift for the waters, lightbar comes along with mouts and brackets!! This vehicle also comes with 200,000 mile Warranty!\
+            Hurry before some one takes your chance, this Baby wont' last long in HERE!! ",
+            retailPrice: '$59,999',
+            stock: 1
+        },
+        ////////////////////////////////////////////////FORDS///////////////////////////////////////////////////////
+        {
+            imgUrl:
+                'http://www.stage3motorsports.com/assets/images/Blog%20Images/2015%20F150%20ANZO%20Lights/Anzo%202015%20F150%20Headlights%20Installed%2000.jpg',
             year: '2014',
             make: 'Ford',
             model: 'F-150 FX-4',
@@ -69,7 +87,8 @@ var PAGE_DATA = {
             stock: 2
         },
         {
-            imgUrl: '',
+            imgUrl:
+                'https://i.pinimg.com/736x/06/d9/97/06d9975932399c0e83c94957fc980c52--ford-f-lifted-ford-trucks.jpg',
             year: '2014',
             make: 'Ford',
             model: 'F-150 FX-4',
@@ -81,9 +100,86 @@ var PAGE_DATA = {
             you just want to show off, we have it too!! Only At Vargas Offroad!!',
             retailPrice: '$58,500',
             stock: 2
+        },
+        /////////////////////////////////////////////JEEP///////////////////////////////////////////////////////////////////////
+        {
+            imgUrl:
+                'https://i.ebayimg.com/images/g/GCsAAOSwQcJZ5i-3/s-l1600.jpg',
+            year: '2017',
+            make: 'Jeep',
+            model: 'Jeep Wrangler Unlimited Rubicon 4×4',
+            engine: '5.0 SuperCharged',
+            mileage: '891',
+            description:
+                'The exterior includes Custom Paint Accents on Gator Dome Hood, Powder Coated Bumper, Lumar Protection, Custom LED Light Switches, and a custom painted Rugged Ridge Angry Eyes grille on the custom hood. Under the hood is a RIPP Supercharger and we’ve also added\
+                 Flowmaster dual exhaust, custom front & rear bumpers, painted to match pocket flares, and Amp research power boards.',
+            retailPrice: '$74,000',
+            stock: 2
+        },
+        {
+            imgUrl:
+                'https://i.ebayimg.com/images/g/jHkAAOSwhMFZ2MES/s-l1600.jpg',
+            year: '2017',
+            make: 'Jeep',
+            model: 'Wrangler Rubicon 4×4',
+            engine: '5.0 SuperCharged',
+            mileage: '2,031',
+            description:
+                'On the front, we have a Smith Built 9K LB Winch with an ultimate dual steering front Turbolined grumper. We have added Turbolined factory flares over the\
+             tires and a 20” black rear Turbolined Fab Four bumper on the back — complete with tire carrier and DV8 LED taillights. The inside is lined with custom red stitched leather interior. The doors and dash have been given painted to match\
+              accents to unify this Firecracker Red rig.',
+            retailPrice: '$71,600',
+            stock: 2
+        },
+        {
+            imgUrl:
+                'https://s-media-cache-ak0.pinimg.com/originals/3f/c7/f8/3fc7f8af29709a98254aad118c8e7790.jpg',
+            year: '2016',
+            make: 'Jeep',
+            model: 'SRT8 Limited Edition Black-Widow 4X4',
+            engine: '679HP 6.4 Super Turbo-Charged HEMI-like',
+            mileage: '18',
+            description:
+                ' Have a friend that has a Camaro and no one has beaten him yet?? No problem, \
+                this vehicle will not struggle to beat it. 0 - 60 mph in (3.6s)!!Smooth premium leather seats with perforated Ultrasuede inserts,\
+             Active Noise Cancellation, Uconnect 8.4 NAV and much more.',
+            retailPrice: '$91,000',
+            stock: 3
+        },
+        {
+            imgUrl:
+                'https://ssli.ebayimg.com/images/g/CBIAAOSwpkFY5yV-/s-l640.jpg',
+            year: '2016',
+            make: 'Jeep',
+            model: 'SRT8 Limited Edition Black-Widow 4X4',
+            engine: '679HP 6.4 Super Turbo-Charged HEMI-like',
+            mileage: '14,000',
+            description:
+                ' Have a friend that has a Camaro and no one has beaten him yet?? No problem, \
+                this vehicle will not struggle to beat it. 0 - 60 mph in (3.6s)!!Smooth premium leather seats with perforated Ultrasuede inserts,\
+             Active Noise Cancellation, Uconnect 8.4 NAV and much more.',
+            retailPrice: '$80,000',
+            stock: 3
+        },
+        {
+            imgUrl:
+                'https://i.pinimg.com/736x/6e/d2/6f/6ed26f760c30146762af99f11ba0360c--jeepers-creepers-car-game.jpg',
+            year: '2017',
+            make: 'Jeep',
+            model: 'Wrangler Rucicon 4X4',
+            engine: '700HP 6.0 Super Charged Rubicon',
+            mileage: '3',
+            description:
+                'Who says you can’t stay drenched in luxury in this new post-apocalyptic world? This baby is fused into one completely bad SUV.\
+             The vehicle is packed with a 6.8 liter V10 engine, seating for 6, night vision cameras and bulletproof armor.\
+             Features include long range fuel tanks, the ability to survive TNT explosions, ultra modern climate control system, \
+             run flat tire that can be pierced by 12.7mm bullets, anti blast seats, and so much more.',
+            retailPrice: '$245,500',
+            stock: 1
         }
     ]
 };
+
 // --------------------------------------------------------------- SHOW SITE -------------------------------------------------------//
 function showSiteFront() {
     var HTML = '<div class="site-front"> <h1> WELCOME TO VARGAS OFF-ROAD!</h1>';
@@ -104,16 +200,16 @@ function chevroletVehicles(vehicles) {
         HTML +=
             '<h2>' + vehicles.year + vehicles.make + vehicles.model + '</h2>';
         HTML +=
-            '<h5> Features: <p>' +
+            '<h5> Features: <p> <br><b>Engine:</b>: ' +
             vehicles.engine +
-            '<br>' +
+            '<br> <b>Mileage</b>: ' +
             vehicles.mileage +
             '<br>' +
             vehicles.description +
             '</p></h5>';
         HTML += '<h2>' + vehicles.retailPrice + '<br></h2>';
         HTML += '<h1>' + vehicles.stock + '<h1>';
-        HTML += '<button id="buy-vehicle"> BUY NOW! </button>';
+        HTML += '<button id="buy-vehicle"> BUY NOW! </button><br><hr>';
 
         return HTML;
     }
@@ -128,51 +224,102 @@ function filteredChevrolets() {
     });
 }
 
+// ----------------------------------------------------------- END CHEVROLET VEHICLES ---------------------------------------------//
+
 // -------------------------------------------------------- FORD VEHICLES ---------------------------------------------------------//
 function fordVehicles(vehicles) {
-    return vehicles.make.includes('Ford');
-}
-function filteredfords() {
-    var vehicles = PAGE_DATA.allTrucks;
+    if (vehicles.make.includes('Ford')) {
+        var HTML = '<img src="' + vehicles.imgUrl + '">';
+        HTML +=
+            '<h2>' + vehicles.year + vehicles.make + vehicles.model + '</h2>';
+        HTML +=
+            '<h5> Features: <p>' +
+            vehicles.engine +
+            '<br>' +
+            vehicles.mileage +
+            '<br>' +
+            vehicles.description +
+            '</p></h5>';
+        HTML += '<h2>' + vehicles.retailPrice + '<br></h2>';
+        HTML += '<h1>' + vehicles.stock + '<h1>';
+        HTML += '<button id="buy-vehicle"> BUY NOW! </button><br><hr>';
 
-    for (var i in vehicles) {
-        var vehicle = vehicles[i];
-        var result = vehicles.filter(fordVehicles);
-        $('#ford-link').on('click', function() {
-            for (var v in result) {
-                var filtered = result[v];
-                $('.main-content').html(filtered);
-            }
-        });
+        return HTML;
     }
 }
+function filteredFords() {
+    var vehicles = PAGE_DATA.allTrucks;
 
-// ------------------------------------------------------------ END CHEVROLET VEHICLES ----------------------------------------------//
+    //-------------------------------------//
+    var result = vehicles.map(fordVehicles);
+    $('#ford-link').on('click', function() {
+        $('.main-content').html(result);
+    });
+}
+
+// ------------------------------------------------------------ END FORD VEHICLES ----------------------------------------------//
+
+// ----------------------------------------------------------- JEEP VEHICLES --------------------------------------------------//
+function jeepVehicles(vehicles) {
+    if (vehicles.make.includes('Jeep')) {
+        var HTML = '<img src="' + vehicles.imgUrl + '">';
+        HTML +=
+            '<h2>' + vehicles.year + vehicles.make + vehicles.model + '</h2>';
+        HTML +=
+            '<h5> Features: <p>' +
+            vehicles.engine +
+            '<br>' +
+            vehicles.mileage +
+            '<br>' +
+            vehicles.description +
+            '</p></h5>';
+        HTML += '<h2>' + vehicles.retailPrice + '<br></h2>';
+        HTML += '<h1>' + vehicles.stock + '<h1>';
+        HTML += '<button id="buy-vehicle"> BUY NOW! </button><br><hr>';
+
+        return HTML;
+    }
+}
+function filteredJeeps() {
+    var vehicles = PAGE_DATA.allTrucks;
+
+    //-------------------------------------//
+    var result = vehicles.map(jeepVehicles);
+    $('#jeep-link').on('click', function() {
+        $('.main-content').html(result);
+    });
+}
+
+// ----------------------------------------------------------- END JEEP VEHICLES ----------------------------------------------//
 
 // ---------------------------------------------------- BUY BUTTON --------------------------------------------------------//
+
 function buyButtonHandler() {
-    $('#buy-vehicle').click(function() {
+    $('#buy-vehicle').on('click', function() {
         var path = PAGE_DATA.allTrucks;
 
         for (var i in path) {
             var vehicle = path[i];
 
             if (vehicle.stock != 0) {
+                console.log(vehicle.stock);
                 vehicle.stock -= 1;
-                draw();
             }
         }
     });
 }
+// ------------------------------------------------------------- POST -----------------------------------------------------------//
+function post() {}
 
 //---------------------------------------------------- END BUY BUTTON ----------------------------------------------------------//
 function draw() {
     showSiteFront();
     filteredChevrolets();
+    filteredFords();
+    filteredJeeps();
 }
 
 function main() {
     draw();
-    // filteredChevrolets();
 }
 $('body').on('load', main());
